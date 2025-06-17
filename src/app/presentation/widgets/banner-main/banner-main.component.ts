b133a7ab98fc0e5d9181bedfb78c72f31e5be812
Fussita <hualong.chiang@gmail.com>
2025-06-16 23:09:34 -0400

    redesign

--- src/app/presentation/widgets/banner-main/banner-main.component.ts
+++ src/app/presentation/widgets/banner-main/banner-main.component.ts
@@ -1,10 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, HostListener, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'banner-main',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './banner-main.component.html',
   styleUrl: './banner-main.component.scss'
 })
@@ -12,6 +15,14 @@ export class BannerComponent {
 
   router = inject(Router)
   showMenu = false
+
+  scrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.scrolled = scrollTop > 0; // Puedes ajustar este umbral
+  }
 
   goWhere(route: string) {
     this.router.navigateByUrl(route)
